f3111f92bddafc5afbc208e7cc6eb4988e548513
Semet <hamdanilombok@gmail.com>
2022-07-05 23:38:34 +0800

    change name

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className={styles.title}>Zul</h1>
+				<h1 className={styles.title}>Zulfahmi</h1>
 			</main>
 		</div>
 	);
